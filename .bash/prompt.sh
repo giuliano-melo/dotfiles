00c948bb96691f27f3246ed7e1c57fb404d2658c
Giuliano Melo <gcrm@ig.com.br>
2014-08-03 21:14:07 -0300

    removing old deprecated syntax

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,5 +1,5 @@
 function _git_prompt() {
-    local git_status="`git status -unormal 2>&1`"
+    local git_status="$(git status -unormal 2>&1)"
     if ! [[ "$git_status" =~ Not\ a\ git\ repo ]]; then
         if [[ "$git_status" =~ nothing\ to\ commit ]]; then
             local ansi=32
@@ -13,15 +13,15 @@ function _git_prompt() {
             #test "$branch" != master || branch=' '
         else
             # Detached HEAD.  (branch=HEAD is a faster alternative.)
-            branch="(`git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
-                echo HEAD`)"
+            branch="$(git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
+                echo HEAD)"
         fi
         echo -n 'on \[\e[0;'"$ansi"'m\]'"$branch"'\[\e[0m\] '
     fi
 }
 
 function _prompt_command() {
-    PS1='\[\e[1;37m\]\u@\h\[\e[0m\] \w '"`_git_prompt`"'\[\e[0;31m\]>\[\e[0;33m\]>\[\e[0;32m\]>\[\e[0m\] '
+    PS1='\[\e[1;37m\]\u@\h\[\e[0m\] \w '"$(_git_prompt)"'\[\e[0;31m\]>\[\e[0;33m\]>\[\e[0;32m\]>\[\e[0m\] '
 }
 
 PROMPT_COMMAND=_prompt_command
